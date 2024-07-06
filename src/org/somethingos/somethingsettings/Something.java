51dcfadaa8f43de8603acaa9f189052558bca3bd
DylanAkp <github@dylanakp.dev>
2024-07-06 21:51:39 +0200

    Settings: Enable new design by default

--- src/org/somethingos/somethingsettings/Something.java
+++ src/org/somethingos/somethingsettings/Something.java
@@ -42,7 +42,7 @@ public class Something extends SettingsPreferenceFragment {
 
     private void onSetPrefCard() {
         final boolean newDesign = Settings.Secure.getInt(getContext().getContentResolver(),
-                Settings.Secure.NEW_SETTINGS_LAYOUT, 0) == 1;
+                Settings.Secure.NEW_SETTINGS_LAYOUT, 1) == 1;
         final PreferenceScreen screen = getPreferenceScreen();
         final int count = screen.getPreferenceCount();
         for (int i = 0; i < count; i++) {
